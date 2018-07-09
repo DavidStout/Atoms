822850a8c20c7a77480dec6c8c6c7f4f0034cdfe
David Stout <davestout79@gmail.com>
2018-07-09 15:58:00 -0600

    Add files via upload

--- Possible_question_pairs/For_Loop_Conditions.c
+++ Possible_question_pairs/For_Loop_Conditions.c
@@ -10,6 +10,14 @@ void for_loop_cond_C()
     }
 }
 
+void for_loop_cond_NC()
+{
+    for(int var = 0; var != 1; var = var + 1)
+    {
+        printf("%d\n", var);
+    }
+}
+
 void for_loop_cond2_C()
 {
     char var = 'a';
@@ -17,24 +25,6 @@ void for_loop_cond2_C()
     {    
         printf("%c\n", var);
         var = var + 1;
-    }
-}
-
-void for_loop_cond3_C()
-{
-    int var = 0;
-    for(;var != 4;)
-    {    
-        printf("%d\n", var);
-        var = var + 2;
-    }
-}
-
-void for_loop_cond_NC()
-{
-    for(int var = 0; var != 1; var = var + 1)
-    {
-        printf("%d\n", var);
     }
 }
 
@@ -46,9 +36,19 @@ void for_loop_cond2_NC()
     }
 }
 
+void for_loop_cond3_C()
+{
+    int var = 2;
+    for(;var != 4;)
+    {    
+        printf("%d\n", var);
+        var = var + 2;
+    }
+}
+
 void for_loop_cond3_NC()
 {
-    for(int var = 0; var != 4; var = var + 2)
+    for(int var = 2; var != 4; var = var + 2)
     {
         printf("%d\n", var);
     }
