642a36c7a9f5503854782e6197c2675aa70f756f
David Stout <davestout79@gmail.com>
2018-12-03 22:21:52 -0700

    Add files via upload

--- Possible_question_pairs/Recursion.c
+++ Possible_question_pairs/Recursion.c
@@ -1,61 +1,68 @@
 #include <stdio.h>
 
-void recursion_C1(int j)
+void F1(int V1)
 {
-    if(j < 2)
+printf("b: %d\n", V1);
+}
+
+void recursion_C1(int V2)
+{
+    if(V2 < 2)
     {
-        printf("a: %d\n", j);
-        j++;
-        recursion_C1(j);
+        printf("a: %d\n", V2);
+        V2++;
+        recursion_C1(V2);
     }
 }
 
-void recursion_NC1(int j)
+void recursion_NC1(int V3)
 {
-    while(j < 2)
+    while(V3 < 2)
     {
-        printf("b: %d\n", j);
-        j++;
+        F1(V3);
+        V3++;
     }
 }
 
-void recursion_C2(int k)
+void recursion_C2(int V4)
 {
-    if(k <= 2)
+    if(V4 <= 2)
     {
-        printf("c: %d\n", k);
-        k++;
-        recursion_C2(k);
+        printf("c: %d\n", V4);
+        V4++;
+        recursion_C2(V4);
     }
 }
 
-void recursion_NC2(int k)
+void recursion_NC2(int V5)
 {
-    while(k <= 2)
+    while(V5 <= 2)
     {
-        printf("d: %d\n", k);
-        k++;
+        F1(V5);
+        V5++;
     }
 }
 
-void recursion_C3(int i)
+
+void recursion_C3(int V6)
 {
-    if(i > 0)
+    if(V6 > 0)
     {
-        printf("e: %d\n", i);
-        i--;
-        recursion_C3(i);
+        printf("e: %d\n", V6);
+        V6--;
+        recursion_C3(V6);
     }
 }
 
-void recursion_NC3(int i)
+void recursion_NC3(int V7)
 {
-    while(i > 0)
+    while(V7 > 0)
     {
-        printf("f: %d\n", i);
-        i--;
+        F1(V7);
+        V7--;
     }
 }
+
 int main(void)
 {
     recursion_C1(0);
