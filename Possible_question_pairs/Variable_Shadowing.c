778bd15e9109798d96cd436abb7a12a0bb54844d
David Stout <davestout79@gmail.com>
2018-12-23 23:07:29 -0700

    Add files via upload

---
+++ Possible_question_pairs/Variable_Shadowing.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+void Variable_Shadowing_C1()
+{
+	int V1 = 4;
+    if(1)
+    {
+        int V1 = 1;
+        V1++;
+        printf("a: %d\n", V1);
+    }
+    printf("b: %d\n", V1);
+}
+
+void Variable_Shadowing_NC1()
+{
+	int V1 = 4;
+    if(1)
+    {
+        int V2 = 1;
+        V2++;
+        printf("c: %d\n", V2);
+    }
+    printf("d: %d\n", V1);
+}
+
+void Variable_Shadowing_C2()
+{
+	int V1 = 13;
+    if(1)
+    {
+        int V1 = 22;
+        V1++;
+        printf("a: %d\n", V1);
+    }
+    printf("b: %d\n", V1);
+}
+
+void Variable_Shadowing_NC2()
+{
+	int V1 = 13;
+    if(1)
+    {
+        int V2 = 22;
+        V2++;
+        printf("c: %d\n", V2);
+    }
+    printf("d: %d\n", V1);
+}
+
+
+void Variable_Shadowing_C3()
+{
+	int V1 = 10;
+    if(1)
+    {
+        int V1 = 1;
+        V1++;
+        printf("a: %d\n", V1);
+    }
+    printf("b: %d\n", V1);
+}
+
+void Variable_Shadowing_NC3()
+{
+	int V1 = 10;
+    if(1)
+    {
+        int V2 = 1;
+        V2++;
+        printf("c: %d\n", V2);
+    }
+    printf("d: %d\n", V1);
+}
+int main(void)
+{
+	Variable_Shadowing_C1();
+	Variable_Shadowing_NC1();
+	printf("\n");
+	Variable_Shadowing_C2();
+	Variable_Shadowing_NC2();
+	printf("\n");
+	Variable_Shadowing_C3();
+	Variable_Shadowing_NC3();
+}
