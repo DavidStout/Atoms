1d990c86057a53067a99a0f5f721afe73156c1ed
David Stout <davestout79@gmail.com>
2019-02-28 12:58:13 -0700

    Add files via upload

--- Possible_question_pairs/Not_Operator.c
+++ Possible_question_pairs/Not_Operator.c
@@ -1,66 +1,55 @@
 #include <stdio.h>
 
-void Not_Operator_C1()
-{
+void Not_Operator_C1(){
     int V1 = !1;
     printf("a: %d\n", V1);
 }
 
-void Not_Operator_NC1()
-{
+void Not_Operator_NC1(){
     int V2 = 1;
-    if (V2==0)
-    {
+    if (V2==0){
         V2 = 1;
     }
-    else
-    {
+    else{
         V2 = 0;
     }
     printf("b: %d\n", V2);
 }
 
-void Not_Operator_C2()
-{
-    int V1 = !-1;
+void Not_Operator_C2(){
+    int V1 = -1;
+    V1 = !V1;
     printf("a: %d\n", V1);
 }
 
-void Not_Operator_NC2()
-{
+void Not_Operator_NC2(){
     int V2 = -1;
-    if (V2==0)
-    {
+    V2 = !V2;
+    if (V2==0){
         V2 = 1;
     }
-    else 
-    {
+    else{
         V2 = 0;
     }
     printf("b: %d\n", V2);
 }
 
-void Not_Operator_C3()
-{
+void Not_Operator_C3(){
     int V1 = !0;
     printf("a: %d\n", V1);
 }
 
-void Not_Operator_NC3()
-{
+void Not_Operator_NC3(){
     int V2 = 0;
-    if (V2==0)
-    {
+    if (V2==0){
         V2 = 1;
     }
-    else 
-    {
+    else{
         V2 = 0;
     }
     printf("b: %d\n", V2);
 }
-int main(void)
-{
+int main(void){
     Not_Operator_C1();
     Not_Operator_NC1();
     printf("\n");
