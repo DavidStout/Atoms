5ed730ee061a62fbd08d31f092a6335a0f8c4610
David Stout <davestout79@gmail.com>
2019-04-11 13:17:04 -0600

    Add files via upload

--- Possible_question_pairs/Pointer_Math.c
+++ Possible_question_pairs/Pointer_Math.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 void pointer_math_C1(){
-    char *V1 = 1 + "zy";
+    char *V1 = 1 + "93";
     char V2 = *V1;
     printf("%c\n", V2);
 }
 
 void pointer_math_NC1(){
-    char V1[3] = {'z','y', 0};
+    char V1[3] = {'9','3', 0};
     char V2 = V1[1];
     printf("%c\n", V2);
 }
