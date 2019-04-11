6074ef666c8beb1b2b89e41fc622a2ad5760df58
David Stout <davestout79@gmail.com>
2019-04-11 13:19:40 -0600

    Add files via upload

--- Possible_question_pairs/String_assignment.c
+++ Possible_question_pairs/String_assignment.c
@@ -1,49 +1,49 @@
 #include <stdio.h>
 
-void string_literal_C1(){
+void string_assignment_C1(){
     char *V1 = "abcd";
     int V2 = 2;
     printf("%c\n", V1[V2]);
 }
 
-void string_literal_NC1(){
+void string_assignment_NC1(){
     char V1[5] = {'a', 'b', 'c', 'd', 0};
     int V2 = 2;
     printf("%c\n", V1[V2]);
 }
 
-void string_literal_C2(){
+void string_assignment_C2(){
     char *V1 = "1234";
     int V2 = 2;
     printf("%c\n", V1[V2]);
 }
 
-void string_literal_NC2(){
+void string_assignment_NC2(){
     char V1[5] = {'1', '2', '3', '4', 0};
     int V2 = 2;
     printf("%c\n", V1[V2]);
 }
 
-void string_literal_C3(){
+void string_assignment_C3(){
     char *V1 = "1a6b";
-    int V2 = 2;
+    int V2 = 3;
     printf("%c\n", V1[V2]);
 }
 
-void string_literal_NC3(){
+void string_assignment_NC3(){
     char V1[5] = {'1', 'a', '6', 'b', 0};
-    int V2 = 2;
+    int V2 = 3;
     printf("%c\n", V1[V2]);
 }
 
 int main(){
     printf("a:");
-    string_literal_C1();
-    string_literal_NC1();
+    string_assignment_C1();
+    string_assignment_NC1();
     printf("b:");
-    string_literal_C2();
-    string_literal_NC2();
+    string_assignment_C2();
+    string_assignment_NC2();
     printf("c:");
-    string_literal_C3();
-    string_literal_NC3();
+    string_assignment_C3();
+    string_assignment_NC3();
 }
